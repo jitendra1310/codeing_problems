baf00ef00d55832131bf624ff2989b20fadf87c7
jitendra1310 <jitendra.g@homelane.com>
2020-10-13 12:44:11 +0530

    Get The balanced Brackets Using Stack

---
+++ code/Stack/Balanced-Brackets.py
@@ -0,0 +1,33 @@
+"""
+Check The 
+
+"""
+class balancedBrackets:
+    
+    def __init__(self):
+        self.stack = []
+    
+    def checkExp(self,expression):
+        expression = list(expression)
+        res  = True
+        for i in range(0,len(expression)):
+            value = expression[i]
+            if value in ['[','(','{']:
+                self.stack.append(value)
+            elif value in [')','}',']']:                        
+                if value == ')' and self.stack[-1] == '(':
+                    self.stack.pop()
+                elif value == '}' and self.stack[-1] == '{':
+                    self.stack.pop()
+                elif value == ']' and self.stack[-1] == '[':
+                    self.stack.pop() 
+                else:
+                    return False
+        
+        if(len(self.stack) >0):
+            return False
+        else:
+            return True
+       
+obj = balancedBrackets()            
+print(obj.checkExp('[()()]{'))
